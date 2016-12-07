0024a874211d2b16ca66cb3cfeb70e7ded5053e9
nealyi <nealyi@163.com>
2016-12-07 14:07:38 +0800

    Create CrimeListFragment.

--- app/src/main/java/com/nealyi/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/nealyi/criminalintent/CrimeActivity.java
@@ -1,9 +1,6 @@
 package com.nealyi.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
--- app/src/main/java/com/nealyi/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/nealyi/criminalintent/CrimeListFragment.java
@@ -1,10 +1,107 @@
 package com.nealyi.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by nealyi on 16/12/6.
  */
 
 public class CrimeListFragment extends Fragment {
+
+    @BindView(R.id.crime_recycle_view)
+    RecyclerView mCrimeRecycleView;
+
+    CrimeAdapter mAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        ButterKnife.bind(this, view);
+        mCrimeRecycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
+        return view;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecycleView.setAdapter(mAdapter);
+    }
+
+
+    class CrimeHolder extends RecyclerView.ViewHolder {
+        private Crime mCrime;
+
+        @BindView(R.id.list_item_crime_title_text_view)
+        TextView mTitleTextView;
+        @BindView(R.id.list_item_crime_date_text_view)
+        TextView mDateTextView;
+        @BindView(R.id.list_item_crime_solved_check_box)
+        CheckBox mSolvedCheckBox;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(), mCrime.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        public void bindCrime(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_crime, parent, false);
+            return new CrimeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.bindCrime(crime);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
 }
+
