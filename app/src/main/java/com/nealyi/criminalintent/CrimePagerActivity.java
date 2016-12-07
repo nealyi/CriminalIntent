e0e38d64bdf9020e7ad3db7323351599e7d40335
nealyi <nealyi@163.com>
2016-12-07 21:25:53 +0800

    Create CrimePagerActivity replace CrimeActivity.

---
+++ app/src/main/java/com/nealyi/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,69 @@
+package com.nealyi.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by nealyi on 16/12/7.
+ */
+
+public class CrimePagerActivity extends FragmentActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.nealyi.criminalintent.crime_id";
+
+    @BindView(R.id.activity_crime_pager_view_pager)
+    ViewPager mViewPager;
+
+    private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+        ButterKnife.bind(this);
+
+        initData();
+        setListener();
+
+    }
+
+    private void initData() {
+        mCrimes = CrimeLab.get(this).getCrimes();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+    }
+
+    private void setListener() {
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+        });
+    }
+}
