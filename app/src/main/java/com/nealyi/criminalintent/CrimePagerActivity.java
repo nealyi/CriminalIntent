96dca8248a35a11b4da9aa88f2b708b8e6b649e2
nealyi <nealyi@163.com>
2016-12-09 15:30:22 +0800

    Change v4 FragmentActivity to v7 AppCompatActivity.

--- app/src/main/java/com/nealyi/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/nealyi/criminalintent/CrimePagerActivity.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,7 +20,7 @@ import butterknife.ButterKnife;
  * Created by nealyi on 16/12/7.
  */
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
 
     private static final String EXTRA_CRIME_ID = "com.nealyi.criminalintent.crime_id";
 
--- app/src/main/java/com/nealyi/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/nealyi/criminalintent/SingleFragmentActivity.java
@@ -3,14 +3,14 @@ package com.nealyi.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by nealyi on 16/12/6.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
